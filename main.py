215409a2422969cf766df3feaa10be39b9faedb5
doha <doha.essam@istnetworks.com>
2025-01-20 11:14:49 +0200

    Removed sensitive token and updated configuration

--- main.py
+++ main.py
@@ -34,50 +34,5 @@ def main():
     except Exception as e:
         print(f"Unexpected error: {e}")
 
-
-    
-        #TEST
-        # call api to get user_id from user email
-    def get_user_id(email:str):
-        JIRA_BASE_URL = "https://istnetworks-dev.atlassian.net"  # or https://jira.your-company.com
-        JIRA_USER_EMAIL = "akhamis@istnetworks.com"  # For Jira Cloud basic auth
-        JIRA_API_TOKEN = "ATATT3xFfGF0vdueykvU3RPNPLXFB889pdZUrZSyXQvpDyST1VUuZFW0o-NWJIaWdg8fAjhZM4L7RRywxjlG5P1fXsmvwgF-a-g31qVXYK8B0GHgZOCnIP-LgcAooCKqGNgxDBMGAetuQNVK-6UZYcVi9NDiwb28HAFMXl8xBO5_8SYzLN5iWmo=262BD91F"
-
-
-        # Authentication for Jira (basic auth example)
-        AUTH = (JIRA_USER_EMAIL, JIRA_API_TOKEN)
-
-        # Headers for JSON content
-        HEADERS = {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        }
-
-        url = f"{JIRA_BASE_URL}/rest/api/3/user/search"
-        
-        params = {"query": email}
-        try:
-            # Perform GET request
-            response = requests.get(url, headers=HEADERS, auth=AUTH,params=params)
-            response.raise_for_status()  # Raise an error for HTTP codes >= 400
-            data = response.json()
-            
-            if data and isinstance(data, list):
-                # Assuming the first result is the correct one
-                user = data[0]
-                return user.get("accountId")
-            
-            print("User not found or invalid response.")
-            return None
-        
-        
-        except requests.RequestException as e:
-            print(f"An error occurred: {e}")
-            return None
-    email = 'akhamis@istnetworks.com'
-    user_id = get_user_id(email)
-    print(user_id)
-    
-
 if __name__ == "__main__":
     main()
